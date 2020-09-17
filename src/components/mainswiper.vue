f7abbafb3628ff8a19202247cf6d61d188c0057a
1875032109 <1875032109@qq.com>
2020-09-17 20:07:44 +0800

    'commit'

---
+++ src/components/mainswiper.vue
@@ -0,0 +1,35 @@
+<template>
+  <swiper>
+    <swiper-item path="/home" activeColor="deepPink">
+      <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">
+      <div slot="item-text">首页</div>
+    </swiper-item>
+    <swiper-item path="/category" activeColor="deepPink">
+      <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg" alt="">
+      <div slot="item-text">分类</div>
+    </swiper-item>
+    <swiper-item path="/profile" activeColor="deepPink">
+      <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg" alt="">
+      <div slot="item-text">我的</div>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+  import swiper from "./swiper";
+  import swiperitem from "./swiperitem";
+  export default {
+    name: "mainswiper",
+    components: {
+      swiper,
+      swiperitem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <swiper>
+    <div slot="left">首页</div>
+  </swiper>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+<script>
+  import swiper from "components/swiper";
+  export default {
+    name: "home",
+    components: {
+      swiper
+    }
   }
-}
 </script>
+
+<style scoped>
+  @import url("//unpkg.com/element-ui@2.13.2/lib/theme-chalk/index.css");
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .swiper {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  .swiper-list {
+    flex: 1;
+  }
+
+  .swiper-list:hover {
+    color: deeppink;
+    cursor: pointer;
+  }
+</style>
